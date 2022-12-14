3be7da370321ab0ec4f038de8d030e051b02390d
HacAtac <jhackworth00@outlook.com>
2022-12-14 12:18:20 -0600

    Test API created + Retry annotations with resiel4j

---
+++ currency-exchange-service/src/main/java/com/hacatac/microservices/controller/CircuitBreakerController.java
@@ -0,0 +1,30 @@
+package com.hacatac.microservices.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger =
+            LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "fallBackResponse")
+    public String sampleApi(){
+        logger.info("SAMPLE API CALL RECEIVED");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return forEntity.getBody();
+    }
+
+    private String fallBackResponse(Exception ex){
+        logger.info("Fallback Response Called");
+        return ex.getMessage();
+    }
+
+}
