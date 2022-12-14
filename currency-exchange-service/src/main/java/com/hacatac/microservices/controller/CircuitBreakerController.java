cf4d641dc4e32e8430a2a46f52aca5f2c363c188
HacAtac <jhackworth00@outlook.com>
2022-12-14 12:43:56 -0600

    Rate Limit && Bulkhead Features of Resilence4j

--- currency-exchange-service/src/main/java/com/hacatac/microservices/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/hacatac/microservices/controller/CircuitBreakerController.java
@@ -1,7 +1,9 @@
 package com.hacatac.microservices.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,18 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
     //@Retry(name = "sample-api", fallbackMethod = "fallBackResponse")
     @CircuitBreaker(name = "default", fallbackMethod = "fallBackResponse")
+    //10s => 10000 calls to the sample api
+    //@RateLimiter(name = "default")
+    //@Bulkhead(name = "sample-api")
     public String sampleApi(){
         logger.info("SAMPLE API CALL RECEIVED");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         return forEntity.getBody();
     }
+//    public String sampleApi(){
+//        logger.info("SAMPLE API CALL RECEIVED");
+//        return "sample-api";
+//    }
 
     private String fallBackResponse(Exception ex){
         logger.info("Fallback Response Called");
