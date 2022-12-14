ec12a2930c2edff3e3040bb2afac69e7553de2fb
HacAtac <jhackworth00@outlook.com>
2022-12-13 18:39:08 -0600

    Set up Dynamic Port in the HTTP GET Response

--- currency-exchange-service/src/main/java/com/hacatac/microservices/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/hacatac/microservices/controller/CurrencyExchangeController.java
@@ -2,6 +2,7 @@ package com.hacatac.microservices.controller;
 
 import com.hacatac.microservices.bean.CurrencyExchange;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,9 @@ public class CurrencyExchangeController {
     @Autowired
     private CurrencyExchange currencyExchange;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable("from")String from,
@@ -24,8 +28,8 @@ public class CurrencyExchangeController {
         currencyExchange.setTo(to);
         currencyExchange.setConversionMultiple(BigDecimal.valueOf(50));
 
-return currencyExchange;
-
-
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 }
